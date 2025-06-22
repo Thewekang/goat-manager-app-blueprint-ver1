a0215ab77ca0ec6fb4a67bfe566f9357d89deb32
Thewekang <nor_hamim@outlook.com>
2025-06-23 04:24:11 +0800

    delete test_flask.py

--- test_flask.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return render_template("login.html")
-if __name__ == "__main__":
-    print("TEMPLATE FOLDER:", app.template_folder)
-    app.run(debug=True)
